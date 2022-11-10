0af369917d394f9d8527b96e7e9421f59a71a08a
Adrian <adrianvalverde45@gmail.com>
2022-11-10 16:12:32 -0500

    commit tree f7f6d2dcc6304fac79c8a875cf720ac15ebc28f6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(function (element){
+        return element.toLowerCase() === name.toLowerCase()
+    })
+};
+
+function fuzzyMatch(drivers, nameLetters) {
+    return drivers.filter(function (element){
+        return element.slice(0,2) == nameLetters
+    })
+};
+
+function matchName(drivers, property){
+    return drivers.filter(function (element){
+        return element.name === property
+    })
+}
